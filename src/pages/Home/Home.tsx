210e58a57e4ffe90b7c1c01c94dc4ae318ace86b
Suzy-Lana777 <reutasvetlana382@gmail.com>
2026-02-11 15:59:46 +0200

    fix header

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,65 +1,3 @@
-// import { Link } from "react-router-dom";
-// import styles from "./Home.module.scss";
-
-// export default function Home() {
-//   return (
-//     <div className={styles.page}>
-//       <div className={styles.container}>
-//         {/* HERO */}
-//         <section className={styles.hero}>
-//           <div className={styles.heroLeft}>
-//             <h1 className={styles.title}>
-//               Unlock your potential with the best{" "}
-//               <span className={styles.language}>language</span> tutors
-//             </h1>
-
-//             <p className={styles.text}>
-//               Embark on an exciting language journey with expert language
-//               tutors. Elevate your language proficiency to new heights by
-//               connecting with highly qualified and experienced tutors.
-//             </p>
-
-//             <Link to="/teachers" className={styles.cta}>
-//               Get started
-//             </Link>
-//           </div>
-
-//           <div className={styles.heroRight}>
-//             <img
-//               src="/images/home-img.png"
-//               alt="LearnLingo hero"
-//               className={styles.heroImg}
-//             />
-//           </div>
-//         </section>
-
-//         {/* STATS */}
-//         <section className={styles.stats}>
-//           <div className={styles.statItem}>
-//             <span className={styles.statNumber}>32,000+</span>
-//             <span className={styles.statLabel}>Experienced tutors</span>
-//           </div>
-
-//           <div className={styles.statItem}>
-//             <span className={styles.statNumber}>300,000+</span>
-//             <span className={styles.statLabel}>5-star tutor reviews</span>
-//           </div>
-
-//           <div className={styles.statItem}>
-//             <span className={styles.statNumber}>120+</span>
-//             <span className={styles.statLabel}>Subjects taught</span>
-//           </div>
-
-//           <div className={styles.statItem}>
-//             <span className={styles.statNumber}>200+</span>
-//             <span className={styles.statLabel}>Tutor nationalities</span>
-//           </div>
-//         </section>
-//       </div>
-//     </div>
-//   );
-// }
-
 import { Link } from "react-router-dom";
 import styles from "./Home.module.scss";
 
@@ -95,7 +33,7 @@ export default function Home() {
             />
             {/* Твоя нова іконка Apple (Mac) під нею */}
             <svg className={styles.appleIcon} width="391" height="304">
-              <use href="/public/icons/sprite.svg#icon-logo-apple"></use>
+              <use href="/icons/sprite.svg#icon-logo-apple"></use>
             </svg>
           </div>
         </section>
