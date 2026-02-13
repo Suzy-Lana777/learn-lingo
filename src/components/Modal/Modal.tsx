1bc9b072456999cc910e8b60c9afc329f609b1ec
Suzy-Lana777 <reutasvetlana382@gmail.com>
2026-02-13 17:38:33 +0200

    fix header

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,3 +1,59 @@
+// import { useEffect } from "react";
+// import { createPortal } from "react-dom";
+// import styles from "./Modal.module.scss";
+
+// type ModalProps = {
+//   onClose: () => void;
+//   children: React.ReactNode;
+// };
+
+// export default function Modal({ onClose, children }: ModalProps) {
+//   const handleBackdropClick = (event: React.MouseEvent<HTMLDivElement>) => {
+//     if (event.target === event.currentTarget) onClose();
+//   };
+
+//   useEffect(() => {
+//     const handleKeyDown = (e: KeyboardEvent) => {
+//       if (e.key === "Escape") onClose();
+//     };
+
+//     document.addEventListener("keydown", handleKeyDown);
+
+//     const prevOverflow = document.body.style.overflow;
+//     document.body.style.overflow = "hidden";
+
+//     return () => {
+//       document.removeEventListener("keydown", handleKeyDown);
+//       document.body.style.overflow = prevOverflow;
+//     };
+//   }, [onClose]);
+
+//   return createPortal(
+//     <div
+//       className={styles.backdrop}
+//       role="dialog"
+//       aria-modal="true"
+//       onClick={handleBackdropClick}
+//     >
+//       <div className={styles.modal}>
+//         <button
+//           type="button"
+//           className={styles.closeBtn}
+//           onClick={onClose}
+//           aria-label="Close modal"
+//         >
+//           <svg width="19" height="19" className={styles.closeIcon}>
+//             <use href="/icons/sprite.svg#icon-close" />
+//           </svg>
+//         </button>
+
+//         {children}
+//       </div>
+//     </div>,
+//     document.body,
+//   );
+// }
+
 import { useEffect } from "react";
 import { createPortal } from "react-dom";
 import styles from "./Modal.module.scss";
@@ -9,12 +65,16 @@ type ModalProps = {
 
 export default function Modal({ onClose, children }: ModalProps) {
   const handleBackdropClick = (event: React.MouseEvent<HTMLDivElement>) => {
-    if (event.target === event.currentTarget) onClose();
+    if (event.target === event.currentTarget) {
+      onClose();
+    }
   };
 
   useEffect(() => {
     const handleKeyDown = (e: KeyboardEvent) => {
-      if (e.key === "Escape") onClose();
+      if (e.key === "Escape") {
+        onClose();
+      }
     };
 
     document.addEventListener("keydown", handleKeyDown);
