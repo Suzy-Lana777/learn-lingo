16377d375e31fe3d7de9d17c70a34d505fbba906
Suzy-Lana777 <reutasvetlana382@gmail.com>
2026-02-27 16:12:36 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,5 @@
-// import { Routes, Route } from "react-router-dom";
-// import { Toaster } from "react-hot-toast";
-// import HomeLayout from "./components/Layout/HomeLayout";
-// import AppLayout from "./components/Layout/AppLayout";
-
-// import Home from "./pages/Home/Home";
-// import Teachers from "./pages/Teachers/Teachers";
-// import Favorites from "./pages/Favorites/Favorites";
-
-// export default function App() {
-//   return (
-//     <>
-//       <Toaster
-//         position="top-right"
-//         toastOptions={{
-//           duration: 3000,
-//           style: {
-//             borderRadius: "12px",
-//             color: "#fff",
-//           },
-//           success: {
-//             style: {
-//               background: "#388e3c",
-//             },
-//           },
-//           error: {
-//             style: {
-//               background: "#d32f2f",
-//             },
-//           },
-//         }}
-//       />
-
-//       <Routes>
-//         {/* HOME (hero header) */}
-//         <Route element={<HomeLayout />}>
-//           <Route path="/" element={<Home />} />
-//         </Route>
-
-//         {/* APP pages (compact header) */}
-//         <Route element={<AppLayout />}>
-//           <Route path="/teachers" element={<Teachers />} />
-//           <Route path="/favorites" element={<Favorites />} />
-//         </Route>
-//       </Routes>
-//     </>
-//   );
-// }
-
 import { Routes, Route, Navigate } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-// Використовуємо один лейаут для всього додатка
 import MainLayout from "./components/Layout/HomeLayout";
 
 import Home from "./pages/Home/Home";
